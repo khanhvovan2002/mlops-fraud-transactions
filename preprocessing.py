5c3569ecfe93c0c4068ac5fb7505d577f2c85d79
Võ Văn Khánh <micolp@MacBook-Air-cua-Vo.local>
2023-10-22 18:56:54 +0700

    fs

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 # Set path for the input
 RAW_DATA_DIR = os.environ["RAW_DATA_DIR"]
@@ -8,41 +9,39 @@ RAW_DATA_FILE = os.environ["RAW_DATA_FILE"]
 raw_data_path = os.path.join(RAW_DATA_DIR, RAW_DATA_FILE)
 
 # Read dataset
-df = pd.read_csv(raw_data_path, sep=",")
+transactions_data = pd.read_csv(raw_transaction_data_path,  parse_dates=['timestamp'])
+user_events_data = pd.read_csv(raw_client_data_path,index_col=0, quotechar="\'", parse_dates=['timestamp'])
+#preproccessing
+transactions_data['month']= transactions_data.timestamp.dt.month
+transactions_data['hour']= transactions_data.timestamp.dt.hour
+fraud_data = transactions_data.loc[transactions_data['fraud'] == 1]
+non_fraud_data = transactions_data.loc[transactions_data['fraud'] == 0]
+category_columns = transactions_data.select_dtypes(include=['object']).columns
+df_cleaned = transactions_data.dropna()
+df_drop = df_cleaned.drop(columns = ['source','target','device','zipcodeOri','zipMerchant'])
+category_columns = df_drop.select_dtypes(include=['object']).columns
+df_encoded = pd.get_dummies(df_drop, columns=category_columns)
+import numpy as np
+C = 2*np.pi/12
+C_ = 2*np.pi/24
+# Map month to the unit circle.
+df_encoded["month_sin"] = np.sin(df_encoded['month']*C)
+df_encoded["month_cos"] = np.cos(df_encoded['month']*C)
+df_encoded.timestamp = df_encoded.timestamp.values.astype(np.int64) // 10 ** 6
+df_encoded['hour_sin']=np.sin(df_encoded['hour']*C)
+df_encoded['hour_cos']=np.cos(df_encoded['hour']*C)
+df = df_encoded.drop(columns = ['month', 'hour'])
 
-
-
-# Income to binary
-df['income'].replace(['<=50K','>50K'],[0,1], inplace=True) 
-
-# Drop useless variables
-df.drop('fnlwgt', axis=1, inplace=True)
-df.drop('education.num', axis=1, inplace=True)
-
-# Removing rows with missing data
-df = df.loc[ (df['workclass'] != '?') & (df['occupation'] != '?') & (df['native.country']!= '?')]
-
-
-
-# Split into dependend and independent variables
-X = df.drop('income', axis=1)
-y = df['income'].to_frame()
-
-# Split X into continous variables and categorical variables
-X_continous  = X[['age', 'capital.gain', 'capital.loss', 'hours.per.week']]
-
-X_categorical = X[['workclass', 'education', 'marital.status', 'occupation', 'relationship', 'race',
-                   'sex', 'native.country']]
-
-# Get the dummies
-X_encoded = pd.get_dummies(X_categorical)
-
-# Concatenate data
-data = pd.concat([y, X_continous, X_encoded],axis=1)
 
 # Split into train and test
-train, test = train_test_split(data, test_size=0.3, stratify=data['income'])
+# Standardize the input features
+scaler = StandardScaler()
+# X, y = df.drop(columns=['fraud']), df['fraud']
 
+X = scaler.fit_transform(X)
+
+# Split the dataset into training and testing sets
+train, test = train_test_split(data, test_size=0.3, stratify=X['fraud'])
 
 # Set path to the outputs
 PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
